8df9c0cccbeceaa76339eb6e56587d49d2d5b9ee
woosang1 <wsl2919@naver.com>
2025-04-27 16:28:25 +0900

    feat: 게스트 스토리 UI 수정

--- feature/home/src/main/java/com/example/home/layout/StoryList.kt
+++ feature/home/src/main/java/com/example/home/layout/StoryList.kt
@@ -1,5 +1,7 @@
 package com.example.home.layout
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,13 +14,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -28,6 +34,10 @@ import com.example.designsystem.theme.LocalColors
 import com.example.designsystem.theme.LocalTypography
 import com.example.model.ui.StoryItem
 import com.example.resource.R as ResourceR
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Brush
+import kotlinx.coroutines.delay
 
 @Composable
 fun StoryList(
@@ -41,27 +51,100 @@ fun StoryList(
         contentPadding = PaddingValues(horizontal = 8.dp),
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        items(stories) { story ->
-            StoryItem(story)
+        itemsIndexed(stories) { index, story ->
+            if (index == 0) {
+                // 첫 번째 항목: 자신의 계정 + 버튼
+                OwnStoryItem(story)
+            } else {
+                // 나머지 항목: 무지개 테두리
+                GuestStoryItem(story)
+            }
         }
     }
 }
 
 @Composable
-fun StoryItem(story: StoryItem) {
+fun OwnStoryItem(story: StoryItem) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box {
+            Box(
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(CircleShape)
+                    .border(
+                        width = 2.dp,
+                        color = LocalColors.current.lightGray,
+                        shape = CircleShape
+                    )
+                    .padding(6.dp)
+            ) {
+                AsyncImage(
+                    model = story.profileImageUrl,
+                    contentDescription = "Story thumbnail",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop,
+                    placeholder = painterResource(ResourceR.drawable.placeholder)
+                )
+            }
+
+            // + 버튼
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .size(20.dp)
+                    .background(
+                        color = LocalColors.current.white,
+                        shape = CircleShape
+                    )
+                    .border(1.dp, color = LocalColors.current.lightGray, shape = CircleShape)
+            ) {
+                Image(
+                    painter = painterResource(ResourceR.drawable.plus),
+                    contentDescription = "Add Story",
+                    modifier = Modifier.align(Alignment.Center),
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(
+            modifier = Modifier.padding(horizontal = 12.dp),
+            text = story.userName,
+            color = LocalColors.current.white,
+            style = LocalTypography.current.body1,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@Composable
+fun GuestStoryItem(story: StoryItem) {
+    // 붉고 노랑 그라데이션 테두리
+    val gradientBorderColor = remember {
+        Brush.linearGradient(
+            colors = listOf(Color.Red, Color.Yellow)
+        )
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
             modifier = Modifier
-                .size(70.dp)
+                .size(80.dp)
                 .clip(CircleShape)
                 .border(
-                    width = 2.dp,
-                    color = LocalColors.current.lightGray,
+                    width = 3.dp,
+                    brush = gradientBorderColor,
                     shape = CircleShape
                 )
-                .padding(3.dp)
+                .padding(6.dp)
         ) {
             AsyncImage(
                 model = story.profileImageUrl,
