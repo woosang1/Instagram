455835db56d9272d8d99328f586cc282809e267b
woosang1 <wsl2919@naver.com>
2025-05-05 11:10:04 +0900

    feat: 프로그래스바 위에 프리뷰 기능 추가

--- core/ui/src/main/java/com/example/ui/component/video/VideoPlayer.kt
+++ core/ui/src/main/java/com/example/ui/component/video/VideoPlayer.kt
@@ -1,18 +1,25 @@
 package com.example.ui.component.video
 
+import android.graphics.BitmapFactory
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.annotation.OptIn
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -46,6 +53,11 @@ import com.example.designsystem.theme.LocalColors
 import com.example.ui.component.licenseUrl
 import kotlinx.coroutines.delay
 import com.example.resource.R as ResourceR
+import androidx.compose.ui.unit.IntOffset
+import androidx.media3.common.util.Log
+import com.example.utils.extension.dpToPixel
+import com.example.utils.log.DebugLog
+import java.io.IOException
 
 
 @OptIn(UnstableApi::class)
@@ -92,8 +104,27 @@ fun VideoPlayer(
     }
     var currentPosition by remember { mutableLongStateOf(0L) }
     var dragProgress by remember { mutableFloatStateOf(-1f) }
+    val assetManager = context.assets
+
+    val thumbnailList = remember {
+        List(10) { index ->
+            val imageName = "thumbnail${String.format("%03d", index)}.jpg"
+            try {
+                val inputStream = assetManager.open(imageName)
+                BitmapFactory.decodeStream(inputStream) ?: run {
+                    // 기본 이미지 반환
+                    BitmapFactory.decodeResource(context.resources, ResourceR.drawable.placeholder)
+                }
+            } catch (e: IOException) {
+                // 이미지 파일을 찾을 수 없거나 다른 오류 발생 시 로그를 출력하고 기본 이미지를 반환
+                Log.e("ThumbnailError", "Error loading image: $imageName", e)
+                BitmapFactory.decodeResource(context.resources, ResourceR.drawable.placeholder)
+            }
+        }
+    }
 
     DisposableEffect(Unit) {
+        DebugLog("thumbnailList : ${thumbnailList.first()}")
         onDispose {
             exoPlayer.release()
         }
@@ -188,43 +219,80 @@ fun VideoPlayer(
         }
 
         // 하단 프로그래스바
-        if (isShowProcessBar){
-            Box(
+        if (isShowProcessBar) {
+            BoxWithConstraints(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(8.dp)
+                    .height(100.dp) // 썸네일 띄울 공간 포함
                     .align(Alignment.BottomCenter)
-                    .pointerInput(Unit) {
-                        detectHorizontalDragGestures(
-                            onDragStart = { offset ->
-                                val ratio = (offset.x / size.width).coerceIn(0f, 1f)
-                                dragProgress = ratio
-                            },
-                            onHorizontalDrag = { change, dragAmount ->
-                                change.consume()
-                                dragProgress = (dragProgress + dragAmount / size.width).coerceIn(0f, 1f)
-                            },
-                            onDragEnd = {
-                                val seekPosition = (exoPlayer.duration * dragProgress).toLong()
-                                exoPlayer.seekTo(seekPosition)
-                                dragProgress = -1f 
-                            },
-                            onDragCancel = {
-                                dragProgress = -1f
-                            }
-                        )
-                    }
             ) {
-                val progress = if (dragProgress >= 0f) dragProgress else currentPosition / exoPlayer.duration.toFloat()
+                val barWidth = constraints.maxWidth.toFloat()
 
-                LinearProgressIndicator(
-                    progress = { progress },
+                Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(8.dp),
-                    color = LocalColors.current.red,
-                    trackColor = LocalColors.current.darkGray,
-                )
+                        .height(8.dp) // 프로그레스 바 높이
+                        .align(Alignment.BottomCenter)
+                        .pointerInput(Unit) {
+                            detectHorizontalDragGestures(
+                                onDragStart = { offset ->
+                                    val ratio = (offset.x / barWidth).coerceIn(0f, 1f)
+                                    dragProgress = ratio
+                                },
+                                onHorizontalDrag = { change, dragAmount ->
+                                    change.consume()
+                                    dragProgress = (dragProgress + dragAmount / barWidth).coerceIn(0f, 1f)
+                                },
+                                onDragEnd = {
+                                    val seekPosition = (exoPlayer.duration * dragProgress).toLong()
+                                    exoPlayer.seekTo(seekPosition)
+                                    dragProgress = -1f
+                                },
+                                onDragCancel = {
+                                    dragProgress = -1f
+                                }
+                            )
+                        }
+                ) {
+                    val progress = if (dragProgress >= 0f) dragProgress else currentPosition / exoPlayer.duration.toFloat()
+
+                    LinearProgressIndicator(
+                        progress = { progress },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(8.dp), // 프로그레스 바 높이
+                        color = LocalColors.current.red,
+                        trackColor = LocalColors.current.darkGray,
+                    )
+                }
+
+                // 썸네일 프리뷰
+                if (dragProgress >= 0f && exoPlayer.duration > 0) {
+                    val previewTimeSec = ((exoPlayer.duration * dragProgress) / 1000).toInt()
+                    val safeIndex = previewTimeSec.coerceIn(0, thumbnailList.lastIndex)
+                    val previewUrl = thumbnailList[safeIndex]
+
+                    Box(
+                        modifier = Modifier
+                            .offset {
+                                IntOffset(
+                                    (dragProgress * barWidth).toInt() - 80, // center align
+                                    (-8).dpToPixel() // 썸네일을 프로그레스 바 위에 8dp 마진을 주어 띄움
+                                )
+                            }
+                            .size(160.dp, 90.dp)
+                            .background(LocalColors.current.black, RoundedCornerShape(8.dp))
+                            .border(1.dp, LocalColors.current.white, RoundedCornerShape(8.dp))
+                            .align(Alignment.TopStart)
+                    ) {
+                        AsyncImage(
+                            model = previewUrl,
+                            contentDescription = "Thumbnail Preview",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
             }
         }
     }
